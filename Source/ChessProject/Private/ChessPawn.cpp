14e537d4d30d05751e23105eedc848721d40af37
fafare56 <fancheventravail@gmail.com>
2025-09-27 23:15:31 -0400

    Initial push

---
+++ Source/ChessProject/Private/ChessPawn.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChessProject/Public/ChessPawn.h"
+
+
+// Sets default values
+AChessPawn::AChessPawn()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AChessPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AChessPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
